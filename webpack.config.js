4bfa00fb8a7e5fe3e0dcece5ecaf7d2afa4a42c4
ar-demko <ag.roza.demko@gmail.com>
2017-03-12 21:21:36 +0100

    budowa środowiska

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  entry: "./js/app.jsx",
+  output: { filename: "./js/out.js" },
+  watch: true,
+  module: {
+    loaders: [
+      {
+        test: /\.jsx$/, exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: { presets: ['es2015', 'react'] }
+      },
+      {
+        test: /\.scss$/,
+        loader: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  }
+}
