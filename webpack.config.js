398c4e76094108271ef0df7eb7cd8eb8e136916d
ar-demko <ag.roza.demko@gmail.com>
2017-03-21 09:07:16 +0100

    zmiana webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,24 @@
 module.exports = {
-  entry: "./js/app.jsx",
-  output: { filename: "./js/out.js" },
-  watch: true,
-  module: {
-    loaders: [
+    entry: ['whatwg-fetch', './js/app.jsx'],
+    output: {
+      filename: './js/out.js'
+     },
+    watch: true,
+    module: {
+      loaders: [
       {
-        test: /\.jsx$/, exclude: /node_modules/,
+        test: /\.jsx$/,
+        exclude: /node_modules/,
         loader: 'babel-loader',
-        query: { presets: ['es2015', 'react'] }
+        query: { presets: ['es2015', 'stage-2', 'react'] }
       },
       {
         test: /\.scss$/,
-        loader: ['style-loader', 'css-loader', 'sass-loader']
+        loaders: ["style-loader", "css-loader", "sass-loader?sourceMap"]
       },
       {
-        test: /\.png$/,
-        loader: 'file-loader'
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000'
       }
     ]
   }
